3efca2b6937fca9fb6d2d2317141c11b6f25093f
thisSasha <thisDevSasha@gmail.com>
2025-07-07 10:52:57 +0300

    Initial commit: Main page

---
+++ js/store.js
@@ -0,0 +1,111 @@
+const products = {
+    'zaptec_go': {
+        'title': 'Zaptec Go',
+        'subtitle': 'Up to 7.4kW charging speed',
+        'description': 'Using leading technology, this award-winning charger is as smart on the inside as it is simple on the outside. It’s Wi-Fi compatible, highly intuitive and available in a choice of six stylish colours',
+        'manufacters_standart': '5-year',
+        'features': ['Up to 7.4kW charging speed', '4G LTE-M or Wi-Fi connectivity'],
+        'applications': ['Holiday parks']
+    },
+
+    'eo_mini_pro_3': {
+        'title': 'EO Mini Pro 3',
+        'subtitle': 'Up to 7.4kW charging speed',
+        'description': 'Compact and stylish in design, this robust device has built-in functionality to allow for load management and solar connectivity. Its simple mobile app is straightforward to use, making charging easy and stress-free.',
+        'manufacters_standart': '3-year',
+        'features': ['Up to 7.4kW charging speed', 'Ethernet, 4G or Wi-Fi connectivity', 'Power balancing capability built in', 'Solar compatible'],
+        'applications': ['Holiday parks']
+    },
+    'eo_genius_2': {
+        'title': 'EO Genius 2',
+        'subtitle': '7.2kW and 22kW variants',
+        'description': 'This device is easy to install and comes with built-in protections required by underwiring regulations, making installation quicker and cheaper than other commercial charge points. An integrated MID meter also allows for different billing options for guests.',
+        'manufacters_standart': '3-year',
+        'features': [
+            '7.2kW and 22kW variants',
+            'Power balancing capability built in',
+            'Ethernet, 4G or Wi-Fi connectivity'
+        ],
+        'applications': [
+            'Holiday homes',
+            'Holiday parks',
+            'Workplaces'
+        ]
+    },
+
+    'zaptec_pro': {
+        'title': 'Zaptec Pro',
+        'subtitle': 'Up to 22kW charging speed',
+        'description': 'With this device, you can create a charging network of multiple charge points off one electrical circuit. With integrated sensors continuously monitoring electricity consumption, it provides a seamless distribution of power among all vehicles, without overloading the facility.',
+        'manufacters_standart': '5-year',
+        'features': [
+            'Up to 22kW charging speed',
+            '4G LTE-M, Wi-Fi or Power Line Communication connectivity',
+            'Dynamic phase allocation',
+            'Dynamic load balancing'
+        ],
+        'applications': [
+            'Apartment buildings',
+            'Workplaces'
+        ]
+    },
+
+    'easee_charge': {
+        'title': 'Easee Charge',
+        'subtitle': 'Up to 22kW charging speed',
+        'description': 'This commercial model retains the sleek and compact look of the home model. Loved by installers for its easy-to-install approach, it’s simple and quick to setup and commission.',
+        'manufacters_standart': '3-year',
+        'features': [
+            'Up to 22kW charging speed',
+            '4G LTE-M or Wi-Fi connectivity',
+            'Scalable system'
+        ],
+        'applications': [
+            'Apartment buildings',
+            'Workplaces'
+        ]
+    },
+
+    'garo_entity_pro': {
+        'title': 'Garo Entity Pro',
+        'subtitle': 'Up to 22kW charging speed',
+        'description': 'The big brother of the Compact, created with a purpose of installing multiple charging stations that work seamlessly together as one network. Available in 4 colours.',
+        'manufacters_standart': '2-year',
+        'features': [
+            'Up to 22kW charging speed',
+            'Smart Phase Balancing',
+            'Wireless Load Management',
+            'Ethernet & 4G connectivity'
+        ],
+        'applications': [
+            'Apartment buildings',
+            'Workplaces'
+        ]
+    },
+
+    'schneider_evlink_pro_ac': {
+        'title': 'Schneider EVlink Pro AC',
+        'subtitle': '7.2kW, 11kW and 22kW variants',
+        'description': 'Highly reliable, flexible and sustainable, this smart charging device is ideal for semi-public parking facilities in commercial and industrial buildings. The device is easy to install, operate, monitor and maintain through digital capabilities and reinforced safety.',
+        'manufacters_standart': '18-month',
+        'features': [
+            '7.2kW, 11kW and 22kW variants',
+            'Ethernet, Bluetooth or 4G connectivity',
+            'NFC functionality',
+            'Compatible with manufacturer’s Building Management systems'
+        ],
+        'applications': [
+            'Workplaces',
+            'Apartment buildings'
+        ]
+    }
+
+};
+
+const how_we_can_help = {
+    'expertise': ['Leaders in residential EV charging solutions', "Backed by the UK Government's Charging Infrastructure Investment Fund (CIIF), Energy Park brings you a team of highly trained professionals."],
+    'guidance': ['A consultative approach', 'We work closely with you throughout the design and installation process to ensure the infrastructure meets your unique requirements'],
+    'support': ['Ongoing management and maintenance', "We can take care of all ongoing operations, including maintenance and customer service, so there's no need for your day-to-day involvement."]
+}
+
+const partner_count = 6;
